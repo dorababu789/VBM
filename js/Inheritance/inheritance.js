bb0c04b06e7dd9c6dee5d7b5a1677c1f64d1384a
dorababu <dorababu.vempatapu@gmail.com>
2020-01-29 15:12:34 +0530

    commited by me

---
+++ js/Inheritance/inheritance.js
@@ -0,0 +1,25 @@
+// var Employee=function(id,name,age,sal,doj){
+//     this.id=id;
+//     this.name=name;
+//     this.age=age;
+//     this.sal=sal;
+//     this.doj=doj;
+//     this.getemployeedetails=function(){
+//         console.log("My employeeid"+" "+this.id+"  "+"name is"+" "+this.name+"  "+"age"+" "+this.age+" " +"Dateofjioning"+" "+(this.doj))
+//     }
+// }
+
+//  var Person=function(sex,height,weight,dob){
+//     this.sex=sex;
+//     this.height=height;
+//     this.weight=weight;
+//     this.dob=dob;
+//     this.getpersondetails=function(){
+//         console.log( "Gender"+" "+this.sex+" "+"Height"+" "+this.height+"cm"+ " "+"Weight"+" "+this.weight+"kg"+"  "+this.dob)
+//     }
+//  }
+//  var  per= new Person( "male", 170,62,"04-04-2020")
+//  per__proto__=new Employee(101,"dora",25,25000,"20-04-1994")
+//  per.getpersondetails()
+//  per__proto__.getemployeedetails()
+
